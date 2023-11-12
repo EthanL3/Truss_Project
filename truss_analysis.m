abe3dbb605167df91e21b7c9b7a68ec53816d1ca
Ethan Levine <101854509+EthanL3@users.noreply.github.com>
2023-11-12 15:24:59 -0500

    Update truss_analysis.m

--- truss_analysis.m
+++ truss_analysis.m
@@ -5,28 +5,33 @@ Ax = zeros(C_rows, C_cols);
 Ay = zeros(C_rows, C_cols);
 %Loop for Ax matrix
 for i = 1:C_rows
+    fprintf('i: %d\n', i);
     members = find(C(i,:)); %columns of C
+    disp('members:');
+    disp(members);
     for j = 1:numel(members)
+        fprintf('j: %d\n', j);
         joints = find(C(:, members(j))); %rows of C
         joints = joints';
+        disp('joints');
+        disp(joints);
         x1 = X(joints(1)); %lower x
         x2 = X(joints(2)); %higher x
         y1 = Y(joints(1)); %lower y
         y2 = Y(joints(2)); %higher y
+        fprintf('x1: %f\n', x1);
+        fprintf('x2: %f\n', x2);
+        fprintf('y1: %f\n', y1);
+        fprintf('y2: %f\n', y2);
         r = sqrt((x2 - x1)^2 + (y2 - y1)^2);
-        if x1 == i %if row# == lower x
-            Ax(i, members(j)) = x2 - x1/r;
-            Ay(i, members(j)) = y2 - y1/r;
+        fprintf('r: %f\n', r);
+        if i == joints(1)
+            Ax(joints(1), members(j)) = x2 - x1/r;
+            Ay(joints(1), members(j)) = y2 - y1/r;
         else
-            Ax(i, members(j)) = x1 - x2/r;
-            Ay(i, members(j)) = y1 - y2/r;
-        end 
+            Ax(joints(2), members(j)) = x1 - x2/r;
+            Ay(joints(2), members(j)) = y1 - y2/r;
+        end
     end
 end
 disp(Ax);
-disp(Ay);
-
-
-
-
-
