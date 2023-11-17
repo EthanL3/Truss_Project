282be56a12bf39e00fefccdc47849d64672180d4
Ethan L <eolev43@gmail.com>
2023-11-16 21:21:54 -0500

    updated truss analysis.m

--- truss_analysis.m
+++ truss_analysis.m
@@ -1,71 +1,81 @@
 load('PracticeProblemInput.mat'); 
 
-[C_rows, C_cols] = size(C);
+[c_rows, c_cols] = size(C);
 
-Ax = zeros(C_rows, C_cols);
-Ay = zeros(C_rows, C_cols);
+Ax = zeros(c_rows, c_cols); 
+Ay = zeros(c_rows, c_cols);
 
-total_length = 0;
-total_cost = 10 * C_rows;
-added_to_length = false;
+total_length = 0; %sum of the lengths of all members
+total_cost = 10 * c_rows; %keeps track of cost
 
-r_vec = [];
-r_index = 1;
+r_vec = zeros(1, c_cols); %to store r values for each member
 
-%Loop for Ax matrix
-for i = 1:C_cols
+w_l = L(find(L)); %Value of load
+%Indexing columns of C matrix
+for i = 1:c_cols
     joints = find(C(:,i));
     x1 = X(joints(1)); %lower x
     x2 = X(joints(2)); %higher x
     y1 = Y(joints(1)); %lower y
     y2 = Y(joints(2)); %higher y
 
-    r = norm([x1,y1]-[x2,y2]);
-
+    r = norm([x1,y1]-[x2,y2]); 
+    r_vec(i) = r;
     total_length = total_length + r;
+
     Ax(joints(1), i)  = (x2 - x1)/r;
     Ax(joints(2), i) = (x1-x2)/r;
     Ay(joints(1), i) = (y2-y1)/r;
     Ay(joints(2), i) = (y1-y2)/r;
-
-   
 end
-total_length = total_length/2;
 total_cost = total_cost + total_length;
 Ax_final = [Ax, Sx];
 Ay_final = [Ay, Sy];
 A = [Ax_final; Ay_final];
 T = A\L;
+T = T';
 
-%Live load:
-x = find(L) - C_rows; %joint x is where load is applied
-x_members = find(Ay(x, :));
-y_force = 0; %Total force upwards opposing the load
-for i = 1:length(x_members)
-    y_force = y_force + (Ay(x, x_members(i)) * T(x_members(i)));
+%Live load
+p_crit = ones(1, c_cols);
+r_m = ones(1, c_cols);
+w_failure = ones(1, c_cols);
+for i = 1:c_cols
+    if T(i) ~= 0
+        r_m(i) = T(i)/w_l;
+    end
+    p_crit(i) = 3654.533 * (r_vec(i)^(-2.119));
+    w_failure(i) = (-1*p_crit(i))/r_m(i);
+    if(abs(w_failure(i)) > 1000)
+        w_failure(i) = 100;
+    end
+    disp(w_failure(i));
 end
-max_load = y_force / 9.8; %theoretical max load
-%Buckling member:
-F = 3654.533 .* buckling_length .^(-2.119); %force of buckling
+
+
+%Critical member info:
+crit_load = min(abs(w_failure));
+crit_member = find(abs(w_failure) == crit_load);
+crit_length = r_vec(crit_member);
+
 
 disp('EK301, Section A6, Ethan L., Margaret H., Micheal M., 11/17/2023');
-disp('Load: 25 N');
+fprintf('Load in oz: %.2f\n', w_l);
 disp('Member forces in oz');
-
 for i = 1:length(T) - 3
-    if T(i) > 0
-        fprintf('m%d: %.2f (C)\n', i, T(i));
-    else
+    if T(i) < 0
+        fprintf('m%d: %.2f (C)\n', i, -1*T(i));
+    elseif T(i) > 0
         fprintf('m%d: %.2f (T)\n', i, T(i));
+    else
+        fprintf('m%d: 0.00\n', i);
     end
 end
-disp('Reaction forces in N:');
+disp('Reaction forces in oz:');
 fprintf('Sx1: %.2f\n', T(length(T)-2));
 fprintf('Sx2: %.2f\n', T(length(T)-1));
 fprintf('Sx3: %.2f\n', T(length(T)));
 fprintf('Total cost: %.2f\n', total_cost);
-fprintf('Theoretical max load/cost ratio in oz/$: %f\n', max_load/total_cost);
-fprintf('Member number: %d\n', buckling_member);
-fprintf('Member length: %f\n', buckling_length);
-fprintf('Predicted buckling strength: %f\n', F);
-disp(r_vec);
+fprintf('Theoretical max load/cost ratio in oz/$: %.2f\n', crit_load/total_cost);
+fprintf('Critical member: %d\n', crit_member);
+fprintf('Critical member length: %.2f\n', crit_length);
+fprintf('Theoretical maximum load (oz): %.2f\n', crit_load);
