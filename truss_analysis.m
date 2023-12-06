cc0faadd6c9f404b42e790b18aa792ebe1088c61
Ethan L <eolev43@gmail.com>
2023-12-05 22:27:30 -0500

    updating

--- truss_analysis.m
+++ truss_analysis.m
@@ -36,26 +36,57 @@ T = A\L;
 T = T';
 
 %Live load
-p_crit = zeros(1, c_cols);
+p_crit_nom = zeros(1, c_cols);
+p_crit_strong = zeros(1, c_cols);
+p_crit_weak = zeros(1, c_cols);
 r_m = ones(1, c_cols);
-w_failure = zeros(1, c_cols);
+w_failure_nom = zeros(1, c_cols);
+w_failure_strong = zeros(1, c_cols);
+w_failure_weak = zeros(1, c_cols);
 for i = 1:c_cols
-    p_crit(i) = 3654.533 * (r_vec(i)^(-2.119));
+    p_crit_nom(i) = 3654.533 * (r_vec(i)^(-2.119));
+    p_crit_strong(i) = 3654.533 * (r_vec(i)^(-2.119)) + 1.685;
+    p_crit_weak(i) = 3654.533 * (r_vec(i)^(-2.119)) - 1.685;
     if T(i) ~= 0
         r_m(i) = T(i)/w_l;
-        w_failure(i) = (-1*p_crit(i))/r_m(i);
+        w_failure_nom(i) = (-1*p_crit_nom(i))/r_m(i);
+        w_failure_strong(i) = (-1*p_crit_strong(i))/r_m(i);
+        w_failure_weak(i) = (-1*p_crit_weak(i))/r_m(i);
     else
-        w_failure(i) = NaN; %if zero-force member, then w_failure is set to NaN
+        %if zero-force member, then w_failure is set to NaN
+        w_failure_nom(i) = NaN; 
+        w_failure_strong(i) = NaN;
+        w_failure_weak(i) = NaN;
     end
 end
 
 
 %Critical member info:
-compressions = w_failure > 0;
-buckling_strengths = w_failure(compressions);
-crit_load = min(buckling_strengths);
-crit_member = find(w_failure == crit_load);
-crit_length = r_vec(crit_member);
+compressions_nom = w_failure_nom > 0;
+compressions_strong = w_failure_strong > 0;
+compressions_weak = w_failure_weak > 0;
+
+buckling_strengths_nom = w_failure_nom(compressions_nom);
+buckling_strengths_strong = w_failure_strong(compressions_strong);
+buckling_strengths_weak = w_failure_weak(compressions_weak);
+
+crit_load_nom = min(buckling_strengths_nom);
+crit_member_nom = find(w_failure_nom == crit_load_nom);
+crit_length_nom = r_vec(crit_member_nom);
+
+crit_load_strong = min(buckling_strengths_strong);
+crit_member_strong = find(w_failure_strong == crit_load_strong);
+crit_length_strong = r_vec(crit_member_strong);
+
+crit_load_weak = min(buckling_strengths_weak);
+crit_member_weak = find(w_failure_weak == crit_load_weak);
+crit_length_weak = r_vec(crit_member_weak);
+
+uncertainty = zeros(1, length(buckling_strengths_nom));
+for i = 1:length(buckling_strengths_nom)
+    uncertainty(i) = buckling_strengths_strong(i) - buckling_strengths_weak(i);
+end
+
 
 
 disp('EK301, Section A6, Ethan L., Margaret H., Micheal M., 11/17/2023');
@@ -70,13 +101,34 @@ for i = 1:length(T) - 3
         fprintf('m%d: 0.00\n', i);
     end
 end
-disp(r_vec)
+
+
 disp('Reaction forces in oz:');
 fprintf('Sx1: %.2f\n', T(length(T)-2));
 fprintf('Sy1: %.2f\n', T(length(T)-1));
 fprintf('Sy2: %.2f\n', T(length(T)));
 fprintf('Total cost: %.2f\n', total_cost);
-fprintf('Theoretical max load/cost ratio in oz/$: %.2f\n', crit_load/total_cost);
-fprintf('Critical member: %d\n', crit_member);
-fprintf('Critical member length: %.2f\n', crit_length);
-fprintf('Theoretical maximum load possible (oz): %.2f\n', crit_load);
+
+fprintf('Nominal buckling strengths:\n');
+disp(buckling_strengths_nom);
+fprintf('Nominal Theoretical max load/cost ratio in oz/$: %.2f\n', crit_load_nom/total_cost);
+fprintf('Nominal Critical member: %d\n', crit_member_nom);
+fprintf('Nominal Critical member length: %.2f\n', crit_length_nom);
+fprintf('Nominal Theoretical maximum load possible (oz): %.2f\n', crit_load_nom);
+
+fprintf('Strong buckling strengths\n')
+disp(buckling_strengths_strong);
+fprintf('Strong Theoretical max load/cost ratio in oz/$: %.2f\n', crit_load_strong/total_cost);
+fprintf('Strong Critical member: %d\n', crit_member_strong);
+fprintf('Strong Critical member length: %.2f\n', crit_length_strong);
+fprintf('Strong Theoretical maximum load possible (oz): %.2f\n', crit_load_strong);
+
+fprintf('Weak buckling strengths\n');
+disp(buckling_strengths_weak);
+fprintf('Nominal Theoretical max load/cost ratio in oz/$: %.2f\n', crit_load_weak/total_cost);
+fprintf('Nominal Critical member: %d\n', crit_member_weak);
+fprintf('Nominal Critical member length: %.2f\n', crit_length_weak);
+fprintf('Nominal Theoretical maximum load possible (oz): %.2f\n', crit_load_weak);
+
+fprintf('Uncertainties:\n');
+disp(uncertainty);
